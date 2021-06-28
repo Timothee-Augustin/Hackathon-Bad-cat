0aa8ce5e85b7ff486beb80f70741e38193c974e2
Timothee <augustin.timothee@gmail.com>
2021-06-28 21:24:42 +0200

    add users route

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,8 +1,11 @@
 const routes = require('express').Router();
+const userRoutes = require('./users');
 
 routes.post('/', (req, res) => {
   console.log('A new request just hit the API !');
   res.send('Hello dear API client :)');
 });
+
+routes.use('/users', userRoutes);
 
 module.exports = routes;

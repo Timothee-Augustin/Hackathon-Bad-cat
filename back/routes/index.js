99a5155e8282ac7e7a8f10231f01dc5a56b95ef5
Timothee <augustin.timothee@gmail.com>
2021-06-28 22:14:36 +0200

    create project routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const userRoutes = require('./users');
 const teamRoutes = require('./teams');
+const projectRoutes = require('./projects');
 
 routes.post('/', (req, res) => {
   console.log('A new request just hit the API !');
@@ -9,5 +10,6 @@ routes.post('/', (req, res) => {
 
 routes.use('/users', userRoutes);
 routes.use('/teams', teamRoutes);
+routes.use('/projects', projectRoutes);
 
 module.exports = routes;

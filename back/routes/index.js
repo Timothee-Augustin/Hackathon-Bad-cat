b29ea94b299ae982561dcd9edbaa3b963a1d587b
Timothee <augustin.timothee@gmail.com>
2021-06-29 11:19:27 +0200

    create login route

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,6 +3,7 @@ const userRoutes = require('./users');
 const teamRoutes = require('./teams');
 const projectRoutes = require('./projects');
 const positionRoutes = require('./positions');
+const loginRoutes = require('./login');
 
 routes.post('/', (req, res) => {
   console.log('A new request just hit the API !');
@@ -13,5 +14,6 @@ routes.use('/users', userRoutes);
 routes.use('/teams', teamRoutes);
 routes.use('/projects', projectRoutes);
 routes.use('/positions', positionRoutes);
+routes.use('/login', loginRoutes);
 
 module.exports = routes;

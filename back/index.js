c53b14765109980994ccfbed9df271010b501f64
Timothee <augustin.timothee@gmail.com>
2021-06-29 12:13:20 +0200

    install cors

--- back/index.js
+++ back/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes);
 
